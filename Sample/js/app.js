39f646379a69a7cca3124647d061c029ca1dd728
ownexperiences <103423774+ownexperiences@users.noreply.github.com>
2022-04-11 15:26:05 +0400

    Create app.js

---
+++ Sample/js/app.js
@@ -0,0 +1,65 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+$(document).ready(function(){    
+  if(localStorage.getItem("logged_in_user") == null)
+    window.location.href = "index.html";
+  // if (localStorage.getItem('access_token') == null )
+  //   document.querySelector(".blur-section").style.display = "flex";
+  // else{
+  //   GetCurrentSpotifyUser();
+  //    document.querySelector(".blur-section").style.display = "none";
+  // }
+});
+
+$(".tabToRecord").on("mousedown",function(){
+  if(!InteractionServices.isRecording && !InteractionServices.isTalking && !InteractionServices.isUploading){
+    InteractionServices.startRecording();    
+  }
+});
+
+$(".tabToRecord").on("mouseup",function(){
+  if(InteractionServices.isRecording){
+    //mediaRecorder.stop();
+    InteractionServices.stopRecording();
+  }
+});
+
+//--------- App Start
+function start(){
+  navigator.mediaDevices.getUserMedia({ audio: true });
+  navigator.permissions.query(
+      { name: 'microphone' }
+  ).then(function(permissionStatus){
+      if(permissionStatus.state == "granted") {
+        navigator.permissions.query(
+          { name: 'geolocation' }
+        ).then(function(permissionStatus){
+            if(permissionStatus.state == "granted"){
+              $(".chatbot").on("touchstart", function(e) {
+                if(!InteractionServices.isRecording && !InteractionServices.isTalking && !InteractionServices.isUploading){
+                  InteractionServices.startRecording();
+                }
+              });
+              $(".chatbot").on("touchend", function(e) {
+                if(InteractionServices.isRecording){
+                  InteractionServices.stopRecording();
+                  $(".tabToRecord").css("opacity","1");
+                }
+              });
+              InteractionServices.connect({ onChange: InteractionServices.printHeartRate }).catch(console.error);
+            } else{
+              alert("Grant Geolocation access please.")
+            } 
+        })
+      } else{
+        alert("Grant Microphone access please.")
+      }
+  })
+}
